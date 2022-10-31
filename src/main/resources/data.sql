9370d005c11cc8b4234c326be11a7fe51fae8d3a
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2022-10-31 20:57:53 +0900

    DBへ接続、データ取り出す

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues(summary, description) values('バグA', 'バグがあります');
+insert into issues(summary, description) values('機能要望B', '機能Bがほしい');
+insert into issues(summary, description) values('画面Cが遅い', '早くしてほしい');
